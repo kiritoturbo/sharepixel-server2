87a63d8987de1f6bb3edde7bc8d594e7b7362372
TruongDev25 <manhtruong2001nt@gmail.com>
2025-01-14 10:24:55 +0700

    cors

--- server.js
+++ server.js
@@ -13,7 +13,8 @@ const port = process.env.PORT || 8888
 
 // Cấu hình CORS
 app.use(cors({
-    origin: 'https://sharepixel-ui.web.app', // URL của frontend
+    // origin: 'https://sharepixel-ui.web.app', // URL của frontend
+    origin: 'https://truestorepixel.com', // URL của frontend
     // origin: 'http://localhost:3000', // URL của frontend
     credentials: true, // Cho phép cookie
 }));
