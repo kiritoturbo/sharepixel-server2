1ac0012f27eb644bdcd8a82eceb0eb54210449d3
TruongDev25 <manhtruong2001nt@gmail.com>
2025-01-20 13:59:41 +0700

    cors

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ app.use(cors({
     // origin: 'https://sharepixel-ui.web.app', // URL của frontend
     origin: 'https://truestorepixel.com', // URL của frontend
     // origin: 'http://localhost:3000', // URL của frontend
-    credentials: true, // Cho phép cookie
+    // credentials: true, // Cho phép cookie
 }));
 app.use(express.json())//cho express đọc được dạng json
 app.use(express.urlencoded({extended:true}))
